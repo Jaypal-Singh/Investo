e37d0d8bfbe6f7e407a41e00fe500b54a7522457
Jaypal-Singh <jaypalsinghchouhan2008@gmail.com>
2025-06-26 19:31:31 +0530

    back hhhhhhhh'

--- Backend/Routes/AuthRoute.js
+++ Backend/Routes/AuthRoute.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const { Signup , Login } = require("../Controllers/AuthController");
 const {signUpValidation,loginValidation} = require("../Middleware/AuthValidation");
 
-router.post("/signup", signUpValidation, Signup);
-router.post("/login", loginValidation, Login);
+router.post("/signup", Signup);
+router.post("/login", Login);
 
 module.exports = router;
